de7fed3d69ce46d6845685cfb5e7d033904b5d8d
Sanket Ghule <sanketbghule@example.com>
2025-01-26 13:09:25 +0530

    Updated pom.xml

--- src/main/java/com/studybuddy/StudyBuddyApplication.java
+++ src/main/java/com/studybuddy/StudyBuddyApplication.java
@@ -1,4 +1,4 @@
-package com.studybuddy.study_buddy;
+package com.studybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
