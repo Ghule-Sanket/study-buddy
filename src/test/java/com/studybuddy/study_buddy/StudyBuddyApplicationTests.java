be85f83c76c07879cabcd7e558da3a0f9c1009c4
Sanket Ghule <sanketbghule@example.com>
2025-01-26 11:29:44 +0530

    Initial commit

---
+++ src/test/java/com/studybuddy/study_buddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studybuddy.study_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
